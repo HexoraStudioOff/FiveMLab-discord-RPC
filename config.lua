ab61e7d49519af3bf6d5adfc8f1ac5336a32ffc1
Hexora Studio <cornetdev@gmail.com>
2026-08-29 15:46:42 +0200

    Add initial configuration settings to config.lua

---
+++ config.lua
@@ -0,0 +1,30 @@
+Config = {}
+
+-- Active ou désactive tous les prints debug
+Config.Debug = false
+
+-- Discord Application ID
+Config.AppId = ************* -- remplace par ton vrai ID
+
+-- Nom affiché
+Config.ServerName = "FiveM Lab RP" -- remplace par ton vrai Nom de serveur
+
+-- URLs des boutons
+Config.StoreUrl = "https://rpsync.tebex.io/" -- remplace par ton vrai store
+Config.DiscordUrl = "https://discord.gg/xRSvsDgpWm" -- remplace par ton vrai discord
+
+-- Assets Discord
+Config.LargeAsset = "logo_big"
+Config.SmallAsset = "logo_small"
+
+-- Temps d'attente avant le premier lancement du RPC côté client
+Config.ClientStartDelay = 5000
+
+-- Temps de refresh du RPC côté client
+Config.ClientRefreshInterval = 15000
+
+-- Temps de refresh du nombre de joueurs côté serveur
+Config.ServerRefreshInterval = 10000
+
+-- Délai avant première update serveur
+Config.ServerStartDelay = 2000
